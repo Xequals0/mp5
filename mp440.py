544411e6f09459291e05039d6fea22e967feaffa
Jimmy Wen <jimmy.wen@eatadakimasu.com>
2018-12-12 19:18:54 -0500

    Working version

--- mp440.py
+++ mp440.py
@@ -33,25 +33,15 @@ def kalman2d(data):
     #Check what the values should be. Already confirmed this should be a 2x1 matrix
     initialGuess = np.array([[1.0],[1.0]])
     xkPrev = initialGuess.dot(myLambda)
-    #holder.append(xkPrev)
 
     u1Prev = data[0][0]
     u2Prev = data[0][1]
     uPrev = np.array([[u1Prev], [u2Prev]])
 
-    #holder.append(initialGuess)
-
     isFirstItem = True
     partialData = data[0:5]
 
-
-    
     for item in data:
-        '''
-        if(isFirstItem):
-            isFirstItem = False
-            continue
-        '''
         u = np.array([[data[0][0]], [data[0][1]]])
         z = np.array([[data[0][2]], [data[0][3]]])
 
@@ -63,16 +53,6 @@ def kalman2d(data):
         kGainNumerator = pk.dot(H_T)
         kGainDenominator = np.add((H.dot(pk)).dot(H_T), R)
         kGain = np.divide(kGainNumerator, kGainDenominator)
-
-        print
-        print "kGain: " + str(kGain)
-        print "z: " + str(z)
-        print "xk: " + str(xk)
-        print "H: " + str(H)
-        print "H.xk: " + str(H.dot(xk))
-        print "Q: " + str(Q)
-        print "R: " + str(R)
-        print
 
         xkPrev = np.add(xk, kGain.dot(np.subtract(z, H.dot(xk))))
         pEstimate = np.subtract(pk, (kGain.dot(H)).dot(pk))
@@ -86,16 +66,7 @@ def kalman2d(data):
         
     for npArray in holder:
         listForm = npArray.tolist()
-        print "listForm: " + str(listForm)
         estimated.append(listForm)
-
-    print "ESTIMATED: " + str(estimated)
-    #print
-    #print "ESTIMATED:"
-    #for pair in estimated:
-    #    print str(pair)
-    #print str(estimated)
-
 
     return estimated
 
@@ -114,68 +85,22 @@ def plot(data, output):
 
     print str(len(output))
     for i in range(0, len(output)):
-        #print str(i)
         k.append(i)
         x1.append(output[i][0][0])
         x2.append(output[i][1][0])
         z1.append(data[i][2])
         z2.append(data[i][3])
-
-    '''
-    pyp.plot(x1, x2, 'b')
-    pyp.plot(z1, z2, 'r')
-    '''
-
     
-    pyp.plot(k, x1, 'b')
-    pyp.plot(k, x2, 'r')
-    pyp.plot(k, z1, 'g')
-    pyp.plot(k, z2, 'm')
-    
-
-
-
-    '''
-    k = []
-    z1 = []
-    z2 = []
-    x1 = []
-    x2 = []
-
-    for i in range(1, len(data)):
-        k.append(i)
-        z1.append(data[i][2])
-        z2.append(data[i][3])
-        #x1.append(output[i])
-
-    for row in output:
-        x1.append(row[0][0])
-        x2.append(row[1][0])
-    '''
-
-    '''
-    print "x1:"
-    for line in x1:
-        print str(line)
-    '''
-    
-    '''
-    pyp.plot(k, z1, 'b')
-    pyp.plot(k, z2, 'b--')
-    pyp.plot(k, x1, 'r')
-    pyp.plot(k, x2, 'r--')
-
-    pyp.plot(k, output[row][0][0], 'g')
-    '''
-    
-    '''
-    pyp.plot(x1, x2, 'r')
-    pyp.plot(z1, z2, 'b')
-    '''
+    lx1 = pyp.plot(k, x1, 'b')
+    lx2 = pyp.plot(k, x2, 'r')
+    lz1 = pyp.plot(k, z1, 'g')
+    lz2 = pyp.plot(k, z2, 'm')
+    pyp.xlabel("k")
+    pyp.ylabel("x, z")
+    pyp.legend(["x1", "x2", "z1", "z2"])
     
     pyp.show()
     # Your code ends here 
-    #_raise_not_defined()
     return
 
 '''
